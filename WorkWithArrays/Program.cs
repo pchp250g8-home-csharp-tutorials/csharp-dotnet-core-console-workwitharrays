3cf28e47faf761ce25f7b4c8440e43666838cf63
Alexander <pchp250g8@tutamail.com>
2025-12-15 14:39:04 +0300

    Добавьте файлы проекта.

---
+++ WorkWithArrays/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Input a count of elements in array");
+Int64.TryParse(Console.ReadLine(), out var nElems);
+var iArray = new int[nElems];
+for (int i = 0; i < nElems; i++)
+{
+    Console.WriteLine("Input an element N {0}", i + 1);
+    int.TryParse(Console.ReadLine(), out iArray[i]);
+}
+Console.WriteLine("Inputed array:");
+for (int i = 0;i < nElems; i++)
+{
+    Console.Write(iArray[i] + " ");
+}
+Console.Read();
